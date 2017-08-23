03d1e0d04e6f7430bec9675f1f17cdb5535b13d7
amor69 <amor.bounechada@gmail.com>
2017-08-23 14:09:14 +0200

    Delete SynchronousCommandBus

--- src/Domain/User/User.php
+++ src/Domain/User/User.php
@@ -14,4 +14,20 @@ class User
     public $id;
     public $firstname;
     public $lastname;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
 }
